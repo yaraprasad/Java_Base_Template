42f8fb546d331e182e8ac0f36f7f92ef51690a07
Prasad Yara <prasad.yara2779@gmail.com>
2025-12-24 15:14:24 +0530

    initial commit

--- src/com/java/threads/BasicThreads.java
+++ src/com/java/threads/BasicThreads.java
@@ -1,9 +1,9 @@
-package com.java.collections;
+package com.java.threads;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class BasicCollections {
+public class BasicThreads {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("India","Pakistan");
