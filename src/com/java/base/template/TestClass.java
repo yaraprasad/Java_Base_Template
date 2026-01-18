23592ab79bfd27f0964a85869c7136460715e123
Prasad Yara <prasad.yara2779@gmail.com>
2026-01-18 20:23:59 +0530

    remove unneccesary files

--- src/com/java/base/template/TestClass.java
+++ src/com/java/base/template/TestClass.java
@@ -1,7 +1,10 @@
 package com.java.base.template;
 
+
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+        System.out.println("Hello");
     }
+
 }
